4aa67089de8014528e9c387e30ced5a2a7a4e99e
Furkan Yıldırım <fy454525@gmail.com>
2024-12-16 20:03:26 +0300

    kurulum ve çalıştırma başarılı.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,13 @@
 import 'package:diet_app_mobile/product/navigator/navigate_route_items.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
-    SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
--- lib/product/navigator/navigate_route_items.dart
+++ lib/product/navigator/navigate_route_items.dart
@@ -1,4 +1,4 @@
-import 'package:diet_app_mobile/views/basic/login/login_view.dart';
+import 'package:diet_app_mobile/views/basic/splash/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:get/get_navigation/src/routes/transitions_type.dart';
@@ -8,13 +8,10 @@ import 'package:get/get_navigation/src/routes/transitions_type.dart';
 class NavigatorRoutes {
   static const String init = "/";
   final List<GetPage<dynamic>>? routes = [
-    GetPage(name: NavigatorRoutes.init, page: () =>
-   const LoginView(),
+    GetPage(name: NavigatorRoutes.init, page: () => const SplashView(),transition: Transition.cupertino,transitionDuration: const Duration(milliseconds: 500),curve: Curves.easeInOut),
+    // GetPage(name: NavigateRoutesItems.unknown.withSlash, page: () => const UnknownScreen(),transition: Transition.cupertino,transitionDuration: const Duration(milliseconds: 500),curve: Curves.easeInOut),
     // StorageServices().loadData(StorageItems.isUserRegistered) == true ?  MainScreen() :
     //  const SplashScreen(),
-     transition: Transition.cupertino,transitionDuration: const Duration(milliseconds: 500),curve: Curves.easeInOut),
-    // GetPage(name: NavigateRoutesItems.unknown.withSlash, page: () => const UnknownScreen(),transition: Transition.cupertino,transitionDuration: const Duration(milliseconds: 500),curve: Curves.easeInOut),
-
   ];
 }
 
